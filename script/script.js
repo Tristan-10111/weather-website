48191d7b96931111952f75d6329a3cce80b07577
Tristan vollenhoven <101865401+Tristan-10111@users.noreply.github.com>
2022-06-10 06:42:30 +0200

    some changes

--- script/script.js
+++ script/script.js
@@ -1,3 +1,28 @@
-let weather = {
-  "apikey": "c97a1cbad377d90d481b6c7e28b64173"
-}   
+const inputTxt = document.querySelector('.inputTxt');
+var button = document.querySelector('.btn');
+const showData = document.querySelector('.showData');
+
+//Get API Key from OpenWeather.org
+const API_Key = "a0e78d3b449db7059df0a38abd3952f8";
+
+button.addEventListener('click', () => {
+    const cityInput = inputTxt.value;
+    console.log(cityInput);
+
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&APPID=${API_Key}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            inputTxt.value = " ";
+          
+            showData.innerHTML = `
+                                <ul>
+                                    <li class="desc">${data.weather[0].description}</li>
+                                    <li class="city">${data.name}</li>
+                                    <li class="temp">${data.main.temp}°c</li>
+                                </ul>
+                                `; 
+
+        });
+
+});
